c1e4be5266af7e718531f46f619f2a05c2026f81
WriteWss <writewss@gmail.com>
2026-04-26 22:27:49 +0500

    Add files via upload

---
+++ FinallyLandingJavaScript.js
@@ -0,0 +1,33 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githubusercontent.com/WriteWss/FinallyLanding/refs/heads/main/vospitanie%20i%20takt.jpg",
+
+  "https://raw.githubusercontent.com/WriteWss/FinallyLanding/refs/heads/main/stone%20pillars.png",
+
+  "https://raw.githubusercontent.com/WriteWss/FinallyLanding/refs/heads/main/davai%20zabudem%20o%20morali.jpg",
+
+  "https://raw.githubusercontent.com/WriteWss/FinallyLanding/refs/heads/main/stone%20castle.png",
+
+  "https://raw.githubusercontent.com/WriteWss/FinallyLanding/refs/heads/main/nenyzniy%20opit.jpg",
+
+  "https://raw.githubusercontent.com/WriteWss/FinallyLanding/refs/heads/main/faces.png"
+
+];
+
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+
+const FACE_NAMES = [
+
+  "upbringing and tact",
+
+  "untitled «stone pillars»",
+
+  "Let's forget about morality",
+
+  "untitled «stone castle»",
+
+  "unnecessary experience",
+
+  "untitled «faces»"
+  
+  ];
